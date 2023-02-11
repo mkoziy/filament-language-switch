494acfd64431a0aba83749baf718764db27e505c
Michael Kozii <michael.koziy@gmail.com>
2023-02-11 13:21:44 +0100

    Add cookies support

--- src/Http/Middleware/SwitchLanguageLocale.php
+++ src/Http/Middleware/SwitchLanguageLocale.php
@@ -4,24 +4,35 @@ namespace BezhanSalleh\FilamentLanguageSwitch\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 class SwitchLanguageLocale
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        $locale = session()->get('locale') ?? $request->get('locale') ?? config('app.locale', 'en');
+        $locale = session()->get('locale') ?? $request->get('locale') ?? $request->cookie('locale') ?? config('app.locale', 'en');
 
         if (array_key_exists($locale, config('filament-language-switch.locales'))) {
             app()->setLocale($locale);
+
+            return $this->addLocaleCookie($next($request), $locale);
         }
 
         return $next($request);
+    }
+
+    private function addLocaleCookie(Response $response, string $locale)
+    {
+        $response->headers->setCookie(new Cookie('locale', $locale));
+
+        return $response;
     }
 }
